2f329920b0df62c2607d545f635aab5c41a5a115
abxxbo <devavcado@gmail.com>
2022-03-16 23:16:23 -0500

    organize code

--- src/awm.c
+++ src/awm.c
@@ -4,48 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "ewmh.h"
+#include "init.h"
+
 #define MAX_OF(a, b)  ((a) > (b) ? (a) : (b))
-#define ARR_SIZEOF(x) (sizeof(x)/sizeof(x[0]))
-
-// wm stuff
-#define WM_NAME "awm" // ...
-
-
-// EWMH atoms that start with NET.
-char* EWMH_ATOMS_NET[] = {
-  "_NET_ACTIVE_WINDOW",
-  "_NET_SUPPORTED",
-  "_NET_WM_NAME",
-  "_NET_WM_STATE",
-  "_NET_WM_STATE_ABOVE",
-  "_NET_WM_STATE_MAXIMIZED_VERT",
-  "_NET_WM_STATE_MAXIMIZED_HORZ"
-};
-
-// Set ewmh atoms (_NET)
-void EWMH_atoms_net(Display* dpy, Window root){
-  // Create atoms if they do not exist -- most efficient way to do this
-  for(int i = 0; i < ARR_SIZEOF(EWMH_ATOMS_NET); i++){
-    XInternAtom(dpy, EWMH_ATOMS_NET[i], False);
-    printf("EWMH NET Atom \"%s\" created.\n", EWMH_ATOMS_NET[i]);
-  }
-  
-  // Set property of atoms
-
-  // Active Window
-
-  // Supported
-  XChangeProperty(dpy, root, XInternAtom(dpy, EWMH_ATOMS_NET[1], True), XInternAtom(dpy, EWMH_ATOMS_NET[1], True),
-                  32, PropModeReplace, EWMH_ATOMS_NET, ARR_SIZEOF(EWMH_ATOMS_NET));
-
-
-  // WM Name
-  XChangeProperty(dpy, root, XInternAtom(dpy, EWMH_ATOMS_NET[2], True), XInternAtom(dpy, EWMH_ATOMS_NET[2], True), 8, 
-                  PropModeReplace, WM_NAME, 3);
-
-  // WM State
-  XChangeProperty(dpy, root, XInternAtom(dpy, EWMH_ATOMS_NET[3], True), 
-}
 
 int main(void){
   Display* dpy;
@@ -53,28 +15,8 @@ int main(void){
   XButtonEvent start;
   XEvent ev;
 
-  // try to connect to display
-  if(!(dpy = XOpenDisplay(0))) return 1;
-
-  // Set EWMH atoms
-  EWMH_atoms_net(dpy, DefaultRootWindow(dpy));
-
-  // Raise windows -- Super+R
-  XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("R")), Mod4Mask,
-          DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
-
-  // Left click - move window around.
-  XGrabButton(dpy, 1, Mod4Mask, DefaultRootWindow(dpy), True,
-              ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
-
-  // Right click -- resize window
-  XGrabButton(dpy, 3, Mod4Mask, DefaultRootWindow(dpy), True,
-              ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
-
-  // Run autostart
-  system("/bin/bash ~/.config/awm/autostart");
-  // Run keybind daemon -- in the future: dont use system
-  system("sxhkd -c ~/.config/awm/sxhkdrc &");
+  init_awm(dpy);
+  on_start();
 
   // Event loop
   start.subwindow = None;
