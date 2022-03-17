385eda367098c8d0188897ddd9cfa51c08ba750b
abxxbo <devavcado@gmail.com>
2022-03-16 22:42:15 -0500

    initial EWMH impl (not finished)

--- src/awm.c
+++ src/awm.c
@@ -1,8 +1,51 @@
 #include <X11/Xlib.h>
+#include <X11/Xatom.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_OF(a, b) ((a) > (b) ? (a) : (b))
+#define MAX_OF(a, b)  ((a) > (b) ? (a) : (b))
+#define ARR_SIZEOF(x) (sizeof(x)/sizeof(x[0]))
+
+// wm stuff
+#define WM_NAME "awm" // ...
+
+
+// EWMH atoms that start with NET.
+char* EWMH_ATOMS_NET[] = {
+  "_NET_ACTIVE_WINDOW",
+  "_NET_SUPPORTED",
+  "_NET_WM_NAME",
+  "_NET_WM_STATE",
+  "_NET_WM_STATE_ABOVE",
+  "_NET_WM_STATE_MAXIMIZED_VERT",
+  "_NET_WM_STATE_MAXIMIZED_HORZ"
+};
+
+// Set ewmh atoms (_NET)
+void EWMH_atoms_net(Display* dpy, Window root){
+  // Create atoms if they do not exist -- most efficient way to do this
+  for(int i = 0; i < ARR_SIZEOF(EWMH_ATOMS_NET); i++){
+    XInternAtom(dpy, EWMH_ATOMS_NET[i], False);
+    printf("EWMH NET Atom \"%s\" created.\n", EWMH_ATOMS_NET[i]);
+  }
+  
+  // Set property of atoms
+
+  // Active Window
+
+  // Supported
+  XChangeProperty(dpy, root, XInternAtom(dpy, EWMH_ATOMS_NET[1], True), XInternAtom(dpy, EWMH_ATOMS_NET[1], True),
+                  32, PropModeReplace, EWMH_ATOMS_NET, ARR_SIZEOF(EWMH_ATOMS_NET));
+
+
+  // WM Name
+  XChangeProperty(dpy, root, XInternAtom(dpy, EWMH_ATOMS_NET[2], True), XInternAtom(dpy, EWMH_ATOMS_NET[2], True), 8, 
+                  PropModeReplace, WM_NAME, 3);
+
+  // WM State
+  XChangeProperty(dpy, root, XInternAtom(dpy, EWMH_ATOMS_NET[3], True), 
+}
 
 int main(void){
   Display* dpy;
@@ -12,6 +55,9 @@ int main(void){
 
   // try to connect to display
   if(!(dpy = XOpenDisplay(0))) return 1;
+
+  // Set EWMH atoms
+  EWMH_atoms_net(dpy, DefaultRootWindow(dpy));
 
   // Raise windows -- Super+R
   XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("R")), Mod4Mask,
